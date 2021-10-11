2919be1576bf4b8429cd95d42cad1913013ff750
Touraj Khatibi <2rajpx@gmail.com>
2021-10-11 03:39:32 +0330

    install vpn

---
+++ install-vpn.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get install network-manager-openvpn -y
+
+echo 'Connect to open vpn and run `sudo apt-get upgrade`'
+
+echo 'After that run `./install-zsh.sh`'
