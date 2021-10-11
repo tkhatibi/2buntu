2f27f196edcdb1e8b0ac2ed0ed0792e866497a69
Touraj Khatibi <2rajpx@gmail.com>
2021-10-11 03:40:35 +0330

    install zsh and oh-my-zsh

---
+++ install-zsh.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt-get install zsh -y
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+cp .zshrc ~/.zshrc
+
+source ~/.zshrc
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+sed -i 's|# source $ZSH/oh-my-zsh.sh|source $ZSH/oh-my-zsh.sh|g' ~/.zshrc
+
+echo 'go to install-nvm-node-yarn.sh'
