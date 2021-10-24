67a7813fed0b55f447d5fef7c98bd811b302caa0
Touraj Khatibi <2rajpx@gmail.com>
2021-10-25 01:28:12 +0330

    add kde import todo list

---
+++ kde/import.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# TODO
+
+# personalization > regional settings > date and time => Tehran
+
+# personalization > regional settings > formats => United states
+
+# appearance > global theme => breeze
+
+# appearance > plasma style => breeze
+
+# appearance > application style > window decoration => breeze
+
+# appearance > colors => breeze light
+
+# workspace > workspace behavior > general behavior
+
+# workspace > workspace behavior > screen edges
+
+# workspace > workspace behavior > screen locking
+
+# workspace > workspace behavior > virtual desktops
+
+# workspace > window management > task switcher
+
+# workspace > shortcuts (ctrl+alt+right/left, ctrl+shift+alt+right/left, meta+tab, alt+capslock)
+
+# network > connections
+
+# hardware > input devices > keyboard > layouts
+
+# hardware > display and monitor > night color
+
+# hardware > power management > energy saving
+
+# terminal
+
+# widgets (allow window control, system load viewer)
+
+# panels
