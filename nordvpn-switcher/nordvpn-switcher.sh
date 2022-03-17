7b584dafaf0b394eaabdc95be630431eab9de486
Touraj Khatibi <2rajpx@gmail.com>
2022-03-18 03:28:28 +0400

    switch nordvpn firewall and killswitch

--- nordvpn-switcher/nordvpn-switcher.sh
+++ nordvpn-switcher/nordvpn-switcher.sh
@@ -6,8 +6,12 @@ echo $STATUS
 
 if [[ "$STATUS" =~ 'Status: Disconnected' ]]; then
     echo 'Connecting...'
-    nordvpn c Cyprus
+    nordvpn set firewall on
+    nordvpn set killswitch on
+    nordvpn c
 else
     echo 'Disconnecting...'
     nordvpn d
+    nordvpn set killswitch off
+    nordvpn set firewall off
 fi
